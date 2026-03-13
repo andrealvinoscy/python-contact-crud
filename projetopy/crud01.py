e43b02a9c408b8becc8bb44f68d59f48f65726b6
Andre Alvino <andrealvinoscy@gmail.com>
2026-03-13 16:17:51 -0300

    Melhorias ao CRUD e tratamento de erros

--- projetopy/crud01.py
+++ projetopy/crud01.py
@@ -4,7 +4,7 @@ import mysql.connector
 conexao = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="(removi)",
+    password="removi",
     database="agenda_contatos"
 )
 
@@ -14,13 +14,15 @@ cursor = conexao.cursor()
 ## ADICIONA CONTATOS (Obriga o usuario a digitar nome e um celular)
 
 def add_contatos():
+
+    print("=== ADICIONAR CONTATOS ===")
+
     nome_contato = input('Nome: ')
     while nome_contato == "":
         nome_contato = input('Nome é obrigatório. Digite novamente: ')
-
     email_contato = input('E-mail: ')
-
     celular_contato = input('Celular: ')
+    
     while celular_contato == "":
         celular_contato = input('Celular é obrigatório. Digite novamente: ')
     
@@ -31,27 +33,47 @@ def add_contatos():
 
     cursor.execute(sql, valores)
     conexao.commit()
+
+    print("Contato adicionado com sucesso!")
 #------------------------------------------------------------------------------
 ##BUSCA OS CONTATOS SALVOS NO SQL
 
 def consulta_contatos():
+    
+    print("=== CONSULTAR CONTATOS ===")
 
     sql = "SELECT * FROM contatos;"
     cursor.execute(sql)
     lista = cursor.fetchall()
     
+    if not lista:
+        print("Nenhum contato cadastrado.")
+        return
     #FORMATAÇÃO VISUAL
     print("ID | Nome | Celular")
     for contatos in lista:
         print(contatos[0], "|", contatos[1], "|", contatos[3])
-    print("Contato adicionado com sucesso!")
 #-------------------------------------------------------------------------------
 
 def editar_contatos():
+
+    print("=== EDITAR CONTATOS ===")
+
     consulta_contatos()
     #ESCOLHER O ID A PARTIR DA LISTA.
     
-    escolha = int(input('Digite o ID do contato a ser editado ou digite 0 para sair: '))
+    try:
+        escolha = int(input('Digite o ID do contato a ser editado: '))
+    except ValueError:
+        print("""ID de contato inválido!
+              Deseja ver a lista novamente?
+              """)
+        
+        escolha_invalida = (input('S/N').upper())
+        if escolha_invalida == 'S':
+            consulta_contatos()
+        
+        return
     
     #SE A ESCOLHA FOR 0, A APLICAÇÃO DEVE ENCERRAR.
     
@@ -73,11 +95,27 @@ def editar_contatos():
     conexao.commit()
     print("Contato atualizado com sucesso!")
 
+#REMOÇÃO DE CONTATOS DA LISTA
 def remover_contato():
+
+    print('=== REMOVER CONTATOS ===')
+
     consulta_contatos()
     
-    escolha = int(input('Digite o ID do contato a ser removido: '))
-    
+    try:
+        escolha = int(input('Digite o ID do contato a ser removido: '))
+    except ValueError:
+        print("""ID de contato inválido!
+              Deseja ver a lista novamente?
+              """)
+        
+        escolha_invalida = (input('S/N').upper())
+        if escolha_invalida == 'S':
+            consulta_contatos()
+        
+        return
+            
+
 
     if escolha == 0:
         return
@@ -90,14 +128,49 @@ def remover_contato():
         conexao.commit()
        
         if cursor.rowcount > 0:
-            print("Contato removido com sucesso!")
+            print("Contato removido com sucesso!")           
         else:
             print('ID não encontrado!')
     else:
         return
-    
 
-<<<<<<< HEAD
-=======
-add_contatos()
->>>>>>> b9eced97825a52f1b2c6426c5e9f68d5fdd0b400
+def menu_inicial():
+    print("""
+    ==== Menu de Contatos ====
+    1- Consultar Contatos
+    2- Adicionar Contatos
+    3- Atualizar Contatos
+    4- Remover Contatos
+    0- Sair
+
+    """)
+#Menu inicial
+while True:  
+    
+    try:
+        menu_inicial()
+        escolha_menu = int(input(":"))
+    except ValueError:
+        print("Digite somente números!")
+        continue
+
+    if escolha_menu == 1:
+        consulta_contatos()
+
+    elif escolha_menu == 2:
+        add_contatos()
+
+    elif escolha_menu == 3:
+        editar_contatos()
+
+    elif escolha_menu == 4:
+        remover_contato()
+
+    elif escolha_menu == 0:
+        print("Saindo...")
+        cursor.close()
+        conexao.close()
+        break
+    else:
+        print("Opção inválida, tente novamente")
+    
